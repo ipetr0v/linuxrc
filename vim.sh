ab3b6ecd48e744a9276f1ef368f7771fb2d4f3a5
Ivan Petrov <ipetr0v@mail.ru>
2019-11-09 22:31:11 +0000

    Update vim

--- vim.sh
+++ vim.sh
@@ -2,6 +2,6 @@
 
 sudo apt install -y vim
 mkdir -p ~/.vim/backup/
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone --depth=1 https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim '+PluginInstall!' +qall
 
